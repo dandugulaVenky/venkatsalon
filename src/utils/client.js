37b3328a1c30cce73a3d84042996ca99a210676e
glasco2534 <technologiesglasco@gmail.com>
2025-03-22 19:49:16 +0530

    resolved unisex error1

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,4 +1,4 @@
-// const baseUrl = "https://easytym-backend.onrender.com";
-const baseUrl = "http://localhost:8800";
+const baseUrl = "https://easytym-backend.onrender.com";
+// const baseUrl = "http://localhost:8800";
 
 export default baseUrl;
