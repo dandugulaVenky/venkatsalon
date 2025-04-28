41da353643de6fea63ae7c7ff22e99294ba09e72
glasco2534 <technologiesglasco@gmail.com>
2025-04-28 17:55:20 +0530

    added headers file for imstagram122

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,67 +1,67 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const CACHE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
 
-const self = this;
+// const self = this;
 
-// Install SW
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Listen for requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
-  );
-});
-
-// Activate the SW
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
-});
-
-// self.addEventListener("notificationclick", (event) => {
-//   console.log(event);
-//   event.notification.close();
-
-//   let clickAction = event.notification.data.click_action || "/";
-
+// // Install SW
+// self.addEventListener("install", (event) => {
 //   event.waitUntil(
-//     clients
-//       .matchAll({
-//         type: "window",
-//       })
-//       .then((clientList) => {
-//         for (let i = 0; i < clientList.length; i++) {
-//           let client = clientList[i];
-//           if (client.url === clickAction && "focus" in client) {
-//             return client.focus();
-//           }
-//         }
-//         if (clients.openWindow) {
-//           return clients.openWindow(clickAction);
-//         }
-//       })
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Opened cache");
+
+//       return cache.addAll(urlsToCache);
+//     })
 //   );
 // });
+
+// // Listen for requests
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request).catch(() => caches.match("offline.html"));
+//     })
+//   );
+// });
+
+// // Activate the SW
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [];
+//   cacheWhitelist.push(CACHE_NAME);
+
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
+
+// // self.addEventListener("notificationclick", (event) => {
+// //   console.log(event);
+// //   event.notification.close();
+
+// //   let clickAction = event.notification.data.click_action || "/";
+
+// //   event.waitUntil(
+// //     clients
+// //       .matchAll({
+// //         type: "window",
+// //       })
+// //       .then((clientList) => {
+// //         for (let i = 0; i < clientList.length; i++) {
+// //           let client = clientList[i];
+// //           if (client.url === clickAction && "focus" in client) {
+// //             return client.focus();
+// //           }
+// //         }
+// //         if (clients.openWindow) {
+// //           return clients.openWindow(clickAction);
+// //         }
+// //       })
+// //   );
+// // });
