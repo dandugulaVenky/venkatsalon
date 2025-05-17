603b4c6649f7bcd0c69a26c0cc1bdf6ed7d8cfa6
glasco2534 <technologiesglasco@gmail.com>
2025-05-17 14:14:16 +0530

    ggegeg4

--- src/pages/login/ForgotPassword.jsx
+++ src/pages/login/ForgotPassword.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import axios from "axios";
+import { auth } from "../../firebase";
 import baseUrl from "../../utils/client";
 import PhoneInput from "react-phone-number-input";
+import { toast } from "react-toastify";
+import { t } from "i18next";
 function ForgotPassword() {
   const [email, setEmail] = useState();
   const [phone, setPhone] = useState();
-
+  const [flag, setFlag] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [result, setResult] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [number, setNumber] = useState("");
+  const [disableNow, setDisableNow] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -32,52 +41,114 @@ function ForgotPassword() {
       });
   };
 
+  function setUpRecaptcha(number) {
+    const recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved - allow signInWithPhoneNumber.
+        },
+        "expired-callback": () => {
+          // Response expired. Ask user to re-enter.
+        },
+      },
+      auth
+    );
+    return recaptchaVerifier.verify().then(() => {
+      return signInWithPhoneNumber(auth, number, recaptchaVerifier);
+    });
+  }
+
+  const getOtp = async (e) => {
+    e.preventDefault();
+    setDisableNow(true);
+    if (!number) return toast("Something is wrong!");
+
+    if (number.toString().length !== 13 || number === undefined) return;
+    try {
+      setDisable(true);
+      const response = await setUpRecaptcha(number);
+      setResult(response);
+      setFlag(true);
+      setDisable(false);
+    } catch (err) {
+      toast(err.message);
+      setFlag(false);
+      setDisable(false);
+    }
+  };
+
+  const verifyOtp = async (e) => {
+    e.preventDefault();
+
+    setDisableNow(false);
+    if (otp === "" || otp === null) return;
+    setDisable(true);
+
+    try {
+      await result.confirm(otp);
+
+      setDisable(false);
+      alert("Phone Verified");
+    } catch (err) {
+      toast.error(`${err.message} please recheck the otp and try again!`);
+      setDisable(false);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-[80vh]">
       <div className="bg-slate-300 p-3 rounded min-w-[30vw]">
         <h4 className="pt-2 pb-5 text-2xl font-bold ">Forgot Password</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3 flex flex-col space-y-2">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              //   autoComplete="off"
-              name="email"
-              className="form-control rounded-0"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          {/* <div className="mb-3 flex flex-col space-y-2">
-            <label htmlFor="phone">
-              <strong>Phone</strong>
-            </label>
-            <input
-              type="number"
-              placeholder="Enter Phone Number"
-              autoComplete="off"
-              name="phone"
-              className="form-control rounded-0"
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div> */}
-
+        <div>
           <div className="mb-4">
             <label htmlFor="phone">Phone</label>
             <PhoneInput
               defaultCountry="IN"
               id="number"
-              value={phone}
-              onChange={setPhone}
+              value={number}
+              onChange={setNumber}
+              disabled={disableNow}
               placeholder="Enter Phone Number"
             />
           </div>
-          <button type="submit" className="primary-button">
-            {loading ? "Sending..." : "Send"}
+          <div id="recaptcha-container"></div>
+          <button
+            className={` ${
+              number?.toString()?.length !== 13 || disable
+                ? "default-button"
+                : "primary-button"
+            } `}
+            onClick={getOtp}
+            disabled={disable || number?.toString()?.length !== 13}
+          >
+            {disable ? "Sending..." : t("getOtp")}
           </button>
-        </form>
+        </div>
+
+        <div
+          style={{
+            display: flag ? "block" : "none",
+          }}
+          className="space-y-2"
+        >
+          <input
+            type="number"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            placeholder="Enter otp"
+            className="w-full"
+          />
+          <div id="recaptcha-container"></div>
+          <button
+            className={`${disable ? "default-button" : "primary-button"}`}
+            onClick={verifyOtp}
+            disabled={disable}
+          >
+            {disable ? "Verifying..." : "Verify"}
+          </button>
+        </div>
       </div>
     </div>
   );
